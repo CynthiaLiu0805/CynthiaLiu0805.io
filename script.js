322390cfdf961540313078a39ff0558761f666e3
Cynthia Liu <liuy363@mcmaster.ca>
2021-12-17 21:52:46 -0500

    add mycv

---
+++ script.js
@@ -0,0 +1,10 @@
+function show() {
+ 
+    /* Access image by id and change
+    the display property to block*/
+    document.getElementById('faceU')
+            .style.display = "block";
+
+    // document.getElementById('btnID')
+    //         .style.display = "none";
+}
